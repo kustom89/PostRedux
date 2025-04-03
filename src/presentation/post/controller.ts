8559b1cf48d006118f4405c7d81af52b84499a1c
Andres <37418332+kustom89@users.noreply.github.com>
2025-04-03 10:59:18 -0300

    fix para busqueda por titulo

--- src/presentation/post/controller.ts
+++ src/presentation/post/controller.ts
@@ -14,28 +14,27 @@ export class PostController {
   public getPostsbyTitle = async (req: Request, res: Response) => {
     const titulo = req.params.titulo;
 
-    const post = await prisma.post.findFirst({
-      where: { titulo: titulo },
-    });
+    const posts: any = await prisma.$queryRaw`
+  SELECT * FROM Post WHERE titulo = ${titulo}
+`;
+    console.log(posts);
 
-    post
-      ? res.json(post)
-      : res.status(404).json({ error: `Post con id ${titulo} no existe` });
+    posts.length > 0
+      ? res.json(posts)
+      : res.status(404).json({ error: `No hay posts con el título ${titulo}` });
   };
 
   public createPost = async (req: Request, res: Response) => {
-    const [error,createPostDto] = CreatePostDto.create(req.body);
+    const [error, createPostDto] = CreatePostDto.create(req.body);
 
     if (error) {
       res.status(400).json({ error });
       return;
     }
 
-
-
     const post = await prisma.post.create({
       data: createPostDto as any,
-          }); 
+    });
 
     res.status(201).json(post);
   };
@@ -43,15 +42,14 @@ export class PostController {
   public updatePost = async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     const [error, updatePostDto] = CreatePostDto.create(req.body);
-    if (error) {  
+    if (error) {
       res.status(400).json({ error });
       return;
     }
-    if (isNaN(id)) {  
+    if (isNaN(id)) {
       res.status(400).json({ error: "Id es requerido" });
       return;
     }
-
 
     const post = await prisma.post.findFirst({
       where: { id },
